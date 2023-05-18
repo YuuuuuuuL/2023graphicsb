491f9a77fd41f5566bd21db8e191c97080ed01a2
YuuuuuuuL <marie920610@gmail.com>
2023-05-18 16:12:55 +0800

    week14

--- Final Project/main.cpp
+++ Final Project/main.cpp
@@ -6,17 +6,19 @@ GLMmodel * head = NULL;
 GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
+GLMmodel * warmR = NULL;
 
-int show[4] = {1, 0, 0, 0};
-
+int show[4] = {1, 1, 1, 1};
+int ID = 2;
 float teapotX = 0, teapotY = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
 void keyboard(unsigned char key, int x, int y){
-    if(key == '0') show[0] =! show[0];
-    if(key == '1') show[1] =! show[1];
-    if(key == '2') show[2] =! show[2];
-    if(key == '3') show[3] =! show[3];
+    if(key == '0') ID = 0;///show[0] =! show[0];
+    if(key == '1') ID = 1;///show[1] =! show[1];
+    if(key == '2') ID = 2;///show[2] =! show[2];
+    if(key == '3') ID = 3;///show[3] =! show[3];
+    if(key == '4') ID = 4;///show[3] =! show[3];
     glutPostRedisplay();
 }
 void display()
@@ -27,16 +29,29 @@ void display()
         body = glmReadOBJ("model/body.obj");
         uparmR = glmReadOBJ("model/rhandup.obj");
         lowarmR = glmReadOBJ("model/rhanddown.obj");
+        warmR = glmReadOBJ("model/rhand.obj");
     }
     glPushMatrix();
         glScalef(0.3, 0.3, 0.3);
         glPushMatrix();
             glTranslatef(teapotX, teapotY, 0);
+
+            if(ID == 0) glColor3f(1, 0, 0);
+            else glColor3f(1, 1, 1);
             if(show[0])glmDraw(head, GLM_MATERIAL);
         glPopMatrix();
-        if(show[1])glmDraw(body, GLM_MATERIAL);
-        if(show[2])glmDraw(uparmR, GLM_MATERIAL);
-        if(show[3])glmDraw(lowarmR, GLM_MATERIAL);
+        if(ID == 1) glColor3f(1, 0, 0);
+            else glColor3f(1, 1, 1);
+            if(show[1])glmDraw(body, GLM_MATERIAL);
+        if(ID == 2) glColor3f(1, 0, 0);
+            else glColor3f(1, 1, 1);
+            if(show[2])glmDraw(uparmR, GLM_MATERIAL);
+        if(ID == 3) glColor3f(1, 0, 0);
+            else glColor3f(1, 1, 1);
+            if(show[3])glmDraw(lowarmR, GLM_MATERIAL);
+        if(ID == 4) glColor3f(1, 0, 0);
+            else glColor3f(1, 1, 1);
+            if(show[4])glmDraw(warmR, GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -61,7 +76,7 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week12");
+    glutCreateWindow("week14");
 
     glutMotionFunc(motion);
     glutDisplayFunc(display);
